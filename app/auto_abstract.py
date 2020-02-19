a05e692c9d074d1f0ea39db4542992c45702ece7
lihongwei <ys.lihongwei@h3c.com>
2020-02-19 14:00:19 +0800

    add files.

---
+++ app/auto_abstract.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
