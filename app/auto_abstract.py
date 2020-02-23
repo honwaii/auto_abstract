eb97cb08e92c16439bb4d4e81c4eb51d08450dc9
Kim <ddyuqing@yahoo.com.tw>
2020-02-23 10:21:28 +0800

    test

--- app/auto_abstract.py
+++ app/auto_abstract.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask,request,url_for, render_template
 
 app = Flask(__name__)
-
-
-@app.route('/')
+@app.route("/")
 def index():
-    return '<h1>Hello World</h1>'
+  return render_template("index.html")
 
+@app.route("/autoabs")
+def auto_abs():
+  return render_template("auto_abs.html",current_language='English')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
