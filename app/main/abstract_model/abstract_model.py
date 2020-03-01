5b5b4e50413ae69cbd57bd495c23f4b85d5ef6a3
honwaii <honwaii@126.com>
2020-03-01 09:25:38 +0800

    update..

--- app/main/abstract_model/abstract_model.py
+++ app/main/abstract_model/abstract_model.py
@@ -1,36 +1,94 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+from functools import reduce
+import pandas as pd
+
+sys.path.append("./src")
 from app.main.abstract_model.src import data_io, SIF_embedding, params
 from app.main.abstract_model.src.params import params
+from app.main.common.cfg_operator import configuration
 
 
 def get_sif_embedding():
-    wordfile = './data/sgns.wiki.word'  # word vector file, can be downloaded from GloVe website
-    weightfile = './auxiliary_data/SogouLabDic.dic'  # each line is a word and its frequency
-    weightpara = 1e-3  # the parameter in the SIF weighting scheme, usually in the range [3e-5, 3e-3]
+    word_file = configuration.get_config('word_file')  # word vector file, can be downloaded from GloVe website
+    # word_file = "./data/sgns.wiki.word"  # word vector file, can be downloaded from GloVe website
+    weight_file = configuration.get_config('weight_file')  # each line is a word and its frequency
+    # weight_file = './auxiliary_data/SogouLabDic.dic'  # each line is a word and its frequency
+    # the parameter in the SIF weighting scheme, usually in the range [3e-5, 3e-3]
+    weight_para = float(configuration.get_config('weight_para'))
     rmpc = 1  # number of principal components to remove in SIF weighting scheme
     # sentences = ['this is an example sentence', 'this is another sentence that is slightly longer']
-    sentences = ['我今天真的很高兴！', '你住在什么地方。']
+    sentences = get_sentences()
 
     # load word vectors 1. 加载词向量文件
-    (words, We) = data_io.getWordmap(wordfile)
+    print("1. 加载词向量文件")
+    (words, We) = data_io.getWordmap(word_file)
     # load word weights  2. 加载权重文件
-    word2weight = data_io.getWordWeight(weightfile, weightpara)  # word2weight['str'] is the weight for the word 'str'
+    print("2. 加载权重文件")
+    word2weight = data_io.getWordWeight(weight_file, weight_para)  # word2weight['str'] is the weight for the word 'str'
     #  3. 计算词向量文件中，每个词所对应的权重
+    print("3. 计算权重")
     weight4ind = data_io.getWeight(words, word2weight)  # weight4ind[i] is the weight for the i-th word
     # load sentences
     # x is the array of word indices, m is the binary mask indicating whether there is a word in that location
     #  4. x : 词的索引  m：表示所在位置是否有一个词
+    print("4. 建立索引")
     x, m = data_io.sentences2idx(sentences, words)
     #  5. 得到各句话中各个词对应的权重的矩阵
+    print("5. 计算权重矩阵")
     w = data_io.seq2weight(x, m, weight4ind)  # get word weights
 
     # set parameters
     params.rmpc = rmpc
 
     # get SIF embedding
+    print("6. 得到句子向量")
     embedding = SIF_embedding.SIF_embedding(We, x, w, params)  # embedding[i,:] is the embedding for sentence i
-    return embedding
+    return sentences, embedding
 
 
-get_sif_embedding()
+def get_sentences():
+    # 0. 从文件读取句子
+    # Columns: [id, author, source, content, feature, title, url]
+    path = configuration.get_config('sentences_path')
+    content = pd.read_csv(path, encoding='gb18030', usecols=['content'], iterator=True)
+    index = 0
+    chunk_size = 500
+    chunks = []
+    loop = True
+    while loop:
+        try:
+            chunk = content.get_chunk(chunk_size)
+            start_index = index * chunk_size
+            for i in range(chunk.size):
+                chunks.append(chunk.at[start_index + i, "content"])
+            index += 1
+        except StopIteration:
+            print("read finish.")
+            loop = False
+    # 1. 拆分句子，先按句号拆分，再按照逗号拆分
+    sentences_temp = []
+    for chunk in chunks:
+        sentences_temp += str(chunk).split("。")
+    sentences = []
+    for sentence in sentences_temp:
+        s = sentence.split("，")
+        sentences += s
+    return sentences
+
+
+def save_sentences_embedding(sentences: list, embedding: list):
+    path = configuration.get_config('embedding_path')
+    output = open(path, 'w')
+    lines = ''
+    for s, vector in zip(sentences, embedding):
+        vector_str = reduce(lambda x, y: str(x) + " " + str(y), vector)
+        lines += (s + " " + vector_str + "\n")
+    output.write(lines)
+    output.close()
+    return
+
+
+s, em = get_sif_embedding()
+save_sentences_embedding(s, em)
