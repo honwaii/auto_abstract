92110324f1e4cbe87d58c786b2c8a33aebc910ba
qinliu <857271391@qq.com>
2020-03-03 00:47:08 +0800

    html flask mysql echarts结合的demo

--- flask_mysql/app.py
+++ flask_mysql/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+import os
+import datetime
+
+import xlwt
+from flask import Flask, render_template, request, send_from_directory
 import json
 
-from pyecharts.charts import Bar
+from pyecharts.charts import Bar, Pie, Line
+from pyecharts import options as opts
 
 from flask_mysql import db
 from jinja2 import Markup
@@ -11,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return hello_world2()
 
 
 @app.route('/hello')
@@ -74,6 +79,18 @@ def read_pvuv_data():
 def pvuv():
     # read file
     data = read_pvuv_data()
+    for datum in data:
+        pdate = datum[0]
+        pv = datum[1]
+        uv = datum[2]
+        sql = f"""
+        insert
+        into
+        pvuv(pdate, pv, uv)
+        values('{pdate}', '{pv}', '{uv}')
+        """
+        print(sql)
+        db.insert_or_update_data(sql)
     # return html
     return render_template("pvuv.html", data=data)
 
@@ -140,6 +157,83 @@ def show_pyecharts():
             .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
     )
     return render_template("show_pyecharts.html", bar_options=bar.dump_options())
+
+
+def get_pie() -> Pie:
+    sql = """
+        select sex, count(1) as cnt from user group by sex
+    """
+    data = db.query_data(sql)
+    c = (
+        Pie()
+            .add("", [(datum['sex'], datum['cnt']) for datum in data])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Pie-基本示例"))
+            .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
+    )
+    return c
+
+
+def get_bar() -> Bar:
+    sql = """
+            select sex, count(1) as cnt from user group by sex
+        """
+    data = db.query_data(sql)
+    c = (
+        Bar()
+            .add_xaxis([datum['sex'] for datum in data])
+            .add_yaxis("数量", [datum['cnt'] for datum in data])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+    )
+    return c
+
+
+def get_line() -> Line:
+    sql = """
+                select pdate, pv, uv from pvuv
+            """
+    data = db.query_data(sql)
+    c = (
+        Line()
+            .add_xaxis([datum['pdate'] for datum in data])
+            .add_yaxis("pv", [datum['pv'] for datum in data])
+            .add_yaxis("商家B", [datum['uv'] for datum in data])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Line-基本示例"))
+    )
+    return c
+
+
+@app.route("/show_myecharts")
+def show_myecharts():
+    pie = get_pie()
+    bar = get_bar()
+    line = get_line()
+    return render_template("show_myecharts.html", pie_options=pie.dump_options(),
+                           bar_options=bar.dump_options(),
+                           line_options=line.dump_options())
+
+
+def generate_excel(data_dir, fname):
+    fpath = os.path.join(data_dir, fname)
+    workbook = xlwt.Workbook(encoding="utf-8")
+    worksheet = workbook.add_sheet("pvuv")
+    for idx, name in enumerate(["日期", "pv", "uv"]):
+        worksheet.write(0, idx, name)
+    data = db.query_data("select * from pvuv")
+    for row, datum in enumerate(data):
+        for col, kv in enumerate(datum.items()):
+            worksheet.write(row + 1, col, kv[1])
+    workbook.save(fpath)
+
+
+@app.route("/download_pvuv_excel")
+def download_pvuv():
+    data_dir = os.path.join(app.root_path, "downloads")
+    now_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    fname = f"pvuv_{now_time}.xls"
+
+    generate_excel(data_dir, fname)
+
+    return send_from_directory(data_dir, fname, as_attachment=True)
 
 
 if __name__ == '__main__':
