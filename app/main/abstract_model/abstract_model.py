ff97cfc9cf6624bee0422471e1e8368e575f8e93
honwaii <honwaii@126.com>
2020-03-04 08:01:02 +0800

    update sentences vector.

--- app/main/abstract_model/abstract_model.py
+++ app/main/abstract_model/abstract_model.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys
-from functools import reduce
-import pandas as pd
 import re
 
-sys.path.append("./src")
-from app.main.abstract_model.src import data_io, SIF_embedding, params
+import pandas as pd
+import numpy as np
+import linecache
+from typing import List
+from functools import reduce
+from sklearn.decomposition import PCA
+from app.main.abstract_model.domain.word import Word
 from app.main.abstract_model.src.params import params
 from app.main.common.cfg_operator import configuration
+from app.main.abstract_model.domain.sentence import Sentence
+from app.main.abstract_model.src import data_io, SIF_embedding, params
 
 
 def get_sif_embedding():
     word_file = configuration.get_config('word_file')  # word vector file, can be downloaded from GloVe website
     # word_file = "./data/sgns.wiki.word"  # word vector file, can be downloaded from GloVe website
-    weight_file = configuration.get_config('weight_file')  # each line is a word and its frequency
+    frequency_file = configuration.get_config('frequency_file')  # each line is a word and its frequency
     # weight_file = './auxiliary_data/SogouLabDic.dic'  # each line is a word and its frequency
     # the parameter in the SIF weighting scheme, usually in the range [3e-5, 3e-3]
     weight_para = float(configuration.get_config('weight_para'))
     rmpc = 1  # number of principal components to remove in SIF weighting scheme
     # sentences = ['this is an example sentence', 'this is another sentence that is slightly longer']
-    sentences = get_sentences()
-    # sentences = ["我喜欢你。", "你是一只小毛驴。"]
-
+    sentences = get_sentences(0, 100, True)
+    # save_sentence(sentences)
     # load word vectors 1. 加载词向量文件
     print("1. 加载词向量文件")
     (words, We) = data_io.getWordmap(word_file)
     # load word weights  2. 加载权重文件
-    print("2. 加载权重文件")
-    word2weight = data_io.getWordWeight(weight_file, weight_para)  # word2weight['str'] is the weight for the word 'str'
+    print("2. 加载词频文件")
+    word2weight = data_io.getWordWeight(frequency_file,
+                                        weight_para)  # word2weight['str'] is the weight for the word 'str'
+    # print(word2weight)
     #  3. 计算词向量文件中，每个词所对应的权重
     print("3. 计算权重")
     weight4ind = data_io.getWeight(words, word2weight)  # weight4ind[i] is the weight for the i-th word
@@ -40,7 +45,6 @@ def get_sif_embedding():
     #  5. 得到各句话中各个词对应的权重的矩阵
     print("5. 计算权重矩阵")
     w = data_io.seq2weight(x, m, weight4ind)  # get word weights
-
     # set parameters
     params.rmpc = rmpc
 
@@ -50,7 +54,29 @@ def get_sif_embedding():
     return sentences, embedding
 
 
-def get_sentences():
+def get_sentences(start: int, size: int, is_all: bool):
+    path = configuration.get_config("processed_sentences")
+    if is_all:
+        with open(path, encoding='utf-8') as reader:
+            sentences_all = reader.read().split("\n")
+            sentences = []
+            for sen in sentences_all:
+                if len(sen) > 0:
+                    sentences.append(sen)
+            return sentences
+    sentences = []
+    if start < 0 or size <= 0:
+        print("参数错误请检查,start={}, size={}", start, size)
+        return sentences
+    linecache.clearcache()
+    for index in range(size):
+        sen = linecache.getline(path, start + index)
+        if len(sen.strip()) > 1:
+            sentences.append(sen.replace("\n", ""))
+    return sentences
+
+
+def handle_essay2sentences():
     # 0. 从文件读取句子
     # Columns: [id, author, result, content, feature, title, url]
     path = configuration.get_config('sentences_path')
@@ -70,12 +96,11 @@ def get_sentences():
                     sentences = cut_sentences(essay)
                     save_sentence(sentences)
             index += 1
-            # if index == 3:
+            # if index == 2:
             #     break
         except StopIteration:
             print("read finish.")
             loop = False
-    # 1. 拆分句子，先按句号拆分，再按照逗号拆分
     sentences_temp = []
     for chunk in chunks:
         sentences_temp += str(chunk).split("。")
@@ -89,27 +114,33 @@ def get_sentences():
 def save_sentences_embedding(sentences: list, embedding: list):
     path = configuration.get_config('embedding_path')
     output = open(path, 'w')
-    lines = ''
+    lines = []
     for s, vector in zip(sentences, embedding):
         vector_str = reduce(lambda x, y: str(x) + " " + str(y), vector)
-        lines += (s + " " + vector_str + "\n")
-    output.write(lines)
+        lines.append(s + " " + vector_str + "\r")
+    output.writelines(lines)
     output.close()
     return
 
 
 def cut_sentences(contents: str):
     contents = contents.replace("\n", "").strip()
-    regex = r"[？！。?!【】,，]"
+    contents = contents.replace("\t", "").strip()
+    contents = contents.replace("\r", "").strip()
+    regex = r"[？！。?!【】,，;；……]"
     sentences_list = re.split(regex, contents)
     sentences = []
-    for s in sentences_list:
+    for index in range(len(sentences_list)):
+        s = sentences_list[index]
         sen_1 = re.subn(r"@(.*)：", "", s)[0].strip()
         sen_2 = re.subn(r"\s", "", sen_1)[0].strip()
         sen = re.subn(r"(（.*）)", "", sen_2)[0].strip()
-        # mini_sens = sen.split("，")
-        # sentences += mini_sens
-        sentences.append(sen + "\n")
+        if len(sen) <= 1 or len(sen.strip()) <= 1:
+            continue
+        if index == len(sentences_list) - 1:
+            sentences.append(sen)
+        else:
+            sentences.append(sen + '\r')
     return sentences
 
 
@@ -117,10 +148,50 @@ def save_sentence(sentences: list):
     path = configuration.get_config("processed_sentences")
     output = open(path, 'a+', encoding='utf-8')
     output.writelines(sentences)
-    output.write("\n")
     output.close()
 
 
-get_sentences()
-# s, em = get_sif_embedding()
-# save_sentences_embedding(s, em)
+def get_word_frequency(word_text, looktable):
+    if word_text in looktable:
+        return looktable[word_text]
+    else:
+        return 1.0
+
+
+# embedding_size = 300
+
+
+def sentence_to_vec(sentence_list: List[Sentence], embedding_size, looktable, a=1e-3):
+    sentence_set = []
+    for sentence in sentence_list:
+        vs = np.zeros(embedding_size)  # add all word2vec values into one vector for the sentence
+        sentence_length = sentence.len()
+        for word in sentence.word_list:
+            a_value = a / (a + get_word_frequency(word.text, looktable))  # smooth inverse frequency, SIF
+            vs = np.add(vs, np.multiply(a_value, word.vector))  # vs += sif * word_vector
+        vs = np.divide(vs, sentence_length)  # weighted average
+        sentence_set.append(vs)  # add to our existing re-calculated set of sentences
+
+    # calculate PCA of this sentence set
+    pca = PCA(n_components=embedding_size)
+    pca.fit(np.array(sentence_set))
+    u = pca.components_[0]  # the PCA vector
+    u = np.multiply(u, np.transpose(u))  # u x uT
+
+    # pad the vector?  (occurs if we have less sentences than embeddings_size)
+    if len(u) < embedding_size:
+        for i in range(embedding_size - len(u)):
+            u = np.append(u, 0)  # add needed extension for multiplication below
+
+    # resulting sentence vectors, vs = vs -u x uT x vs
+    sentence_vecs = []
+    for vs in sentence_set:
+        sub = np.multiply(u, vs)
+        sentence_vecs.append(np.subtract(vs, sub))
+
+    return sentence_vecs
+
+
+handle_essay2sentences()
+s, em = get_sif_embedding()
+save_sentences_embedding(s, em)
---
+++ app/main/abstract_model/domain/word.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/3 0003 20:40
+# @Author  : honwaii
+# @Email   : honwaii@126.com
+# @File    : word.py
+
+
+class Word:
+    def __init__(self, text, vector):
+        self.text = text
+        self.vector = vector
